aacf27bfec0eb384e2b7b4a9db9d280b0dba12ae
Aquib Baig <aquibbaig97@gmail.com>
2020-03-26 18:15:01 +0530

    Insertion sort using Binary search

--- MIT_OpencourseWare/InsertionSort.java
+++ MIT_OpencourseWare/InsertionSort.java
@@ -1,15 +1,18 @@
+package MIT_OpencourseWare;
+
 public class InsertionSort {
     public static void main(String args[]) {
       int arr[] = new int[]{3,5,4,1,2};
       int res[] = new int[5];
       InsertionSorter is = new InsertionSorter();
       res = is.sort(arr);
-      for (int i=0; i<res.length; i++) {
+      for (int i = 0; i < res.length; i++) {
           System.out.print(res[i]);
       }
-    }
-    static class InsertionSorter {
-        public static int[] sort (int[] arr) {
+  }
+
+  static class InsertionSorter {
+      public int[] sort(int[] arr) {
             for (int i=1; i< arr.length; i++) {
                 int key = i;
                 // put arr[i] in the correct position
@@ -26,5 +29,52 @@ public class InsertionSort {
             }
             return arr;
         } 
+    }
+}
+
+// Using Binary Search
+public class InsertionSortBinary {
+    public static int Binarysearch(int[] arr, int el, int lo, int hi) {
+        if (lo < hi) {
+            int mid = (lo + hi)/2;
+            if (el < arr[mid]) {
+                return Binarysearch(arr, el, lo, mid);
+            }
+            else {
+                return Binarysearch(arr, el, mid+1, hi);
+            }
+        } else {
+            return lo;
+        }
+    }
+    public static void printArr(int[] arr) {
+        for (int i=0; i<5; i++) {
+          System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+    public static int[] insertionSort(int[] arr) {
+        for (int i=0; i<arr.length; i++) {
+            if (i==0) {
+                continue;
+            }
+            int pos = Binarysearch(arr, arr[i], 0, i-1);
+            // System.out.println(pos + " :pos");
+            int selected = arr[i];
+            int j = i-1;
+            // Shift other positions
+            while (j>=pos) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[pos] = selected;
+        }
+        return arr;
+    }
+    public static void main(String args[]) {
+      int arr[] = new int[]{5, 4, 3, 1, 2};
+      int res[] = new int[5];
+      res = insertionSort(arr);
+      printArr(res);
     }
 }
